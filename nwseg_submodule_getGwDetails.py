a8c04f1e55f0f7a6091d4bf66a8d1ad9970d97f8
Wehrli <oliver.wehrli@hpe.com>
2020-12-07 17:57:34 +0100

    commit

--- nwseg_submodule_getGwDetails.py
+++ nwseg_submodule_getGwDetails.py
@@ -5,6 +5,9 @@ from pycentral.configuration import Groups
 from pycentral.monitoring import Devices
 from pprint import pprint
 import json
+import csv
+import pandas
+import collections
 
 
 # Create the following files by refering to the samples.
@@ -17,24 +20,53 @@ central = get_conn_from_file(filename=central_filename)
 g = Groups()
 d = Devices()
 
-groups = g.get_groups(central)
+#Import gateway export CSV to pandas DataFrame
 
-#pprint(groups)
-print(groups['msg']['data'])
+df = pandas.read_csv('export_gateway_list_dev.csv', index_col='Device Name')
 
-group_names = groups['msg']['data']
-print(type(group_names))
+print ('Entire pandas DataFrame:')
+print(df)
+
+for index in df.index:
+
+    uptime = df['Uptime'][index]
+    group = df['Group'][index]
+
+    if  uptime != '0:00:00' and group == 'CH-HS-BNC_LAB':
+
+        mac_addr = df['MAC'][index]
+        print(mac_addr)
+        serial = df['Serial'][index]
+        print(serial)
+
+        #device_details = d.get_device_details(central, 'mobility_controllers', f'{serial}')
+        #print(device_details['msg'])
+        #print(type(device_details))
+
+        uplink_details = d.get_gateway_uplink_details(central, 'mobility_controllers', f'{serial}', '3H')
+
+        uplinks = uplink_details['msg']['uplinks']
+        print (type(uplinks))
+
+        keys = uplink_details['msg'].keys()
+        values = uplink_details['msg'].values()
+
+        #print(keys)
+        #print(values)
+
+        values_list = list(values)
+
+        #print(values_list[10])
+        #print(type(values_list))
+
+        #print(uplink_details['msg'][1])
 
 
-#for group, group_name in groups.items():
-#    print("\Group-Name", group)
-
-for key, value in group_dict.items():
-    print(key, '->', value)
+        
+        
 
 
-#device_details = d.get_device_details(central, "mobility_controllers", "CG0046063")
-#print(device_details['msg']['group_name'])
+#pprint(gateway_dict)
 
 #with open('gw-details.json', 'w') as file1:
 #    file1.write(json.dumps(device_details))
